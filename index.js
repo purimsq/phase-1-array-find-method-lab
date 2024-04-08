c7147f3c58d2e36b45ac161d700ad95e2cfc62e1
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-04-09 00:45:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  
+  function superbowlWin(arr){
+    let output = "";
+    arr.find(look);
+  
+    function look(value){
+      return output = value.result === "W" ? value.year : undefined;
+    };
+  
+    return output;
+  };
+  
+  console.log(superbowlWin(record));
